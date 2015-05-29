e21661dca6630079ae962920adffad3a8533f398
haotian-wu <buaawu@gmail.com>
2015-05-29 16:17:33 -0500

    Create maximizing-xor.cpp

---
+++ Warmup/maximizing-xor.cpp
@@ -0,0 +1,51 @@
+/*******************************************************
+ * Copyright (C) 2015 Haotian Wu
+ * 
+ * This file is the solution to the question:
+ * https://www.hackerrank.com/challenges/maximizing-xor
+ * 
+ * Redistribution and use in source and binary forms are permitted.
+ *******************************************************/
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/*
+ * Complete the function below.
+ */
+int maxXor(int l, int r) {
+    int mm=0;
+    for (int i=l;i<=r;i++)
+        for (int j=l;j<=r;j++)
+            if ((i^j)>mm)
+                mm = i^j;
+    return mm;
+}
+
+int main() {
+    int res;
+    int _l;
+    cin >> _l;
+    
+    int _r;
+    cin >> _r;
+    
+    res = maxXor(_l, _r);
+    cout << res;
+    
+    return 0;
+}

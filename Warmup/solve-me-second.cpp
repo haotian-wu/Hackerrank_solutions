f183f58f6894ee8861ecd0de95ac3d1f0eeeab58
haotian-wu <buaawu@gmail.com>
2015-05-29 16:10:14 -0500

    Update and rename solve_me_second.cpp to solve-me-second.cpp

--- Warmup/solve-me-second.cpp
+++ Warmup/solve-me-second.cpp
@@ -1,3 +1,11 @@
+/*******************************************************
+ * Copyright (C) 2015 Haotian Wu
+ * 
+ * This file is the solution to the question:
+ * https://www.hackerrank.com/challenges/solve-me-second
+ * 
+ * Redistribution and use in source and binary forms are permitted.
+ *******************************************************/
 #include <cmath>
 #include <cstdio>
 #include <vector>

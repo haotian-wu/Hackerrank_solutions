558a0338c707fe2625a33a529bcae9e21b85af84
Haotian Wu <buaawu@gmail.com>
2015-06-02 18:14:42 -0500

    Create modified-kaprekar-numbers.cpp

---
+++ Warmup/modified-kaprekar-numbers.cpp
@@ -0,0 +1,7 @@
+/*******************************************************
+ * Copyright (C) 2015 Haotian Wu
+ * 
+ * This file is NOT a solution.
+ *
+ * For the solution to "Modified Kaprekar Numbers", see "kaprekar-numbers.cpp" in this folder.
+ *******************************************************/

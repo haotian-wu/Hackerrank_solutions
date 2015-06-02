bbaa05702f1d45ad88954da27800f779be359d24
Haotian Wu <buaawu@gmail.com>
2015-06-02 15:50:25 -0500

    Create max-min.cpp

---
+++ Warmup/max-min.cpp
@@ -0,0 +1,7 @@
+/*******************************************************
+ * Copyright (C) 2015 Haotian Wu
+ * 
+ * This file is NOT a solution.
+ *
+ * For the solution to "Max min", see "angry-children.cpp" in this folder.
+ *******************************************************/

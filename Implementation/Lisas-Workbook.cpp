ff5ef3aa9293a69a2e0e9ec6e221cc0b7712de12
Haotian Wu <buaawu@gmail.com>
2016-04-19 09:26:36 -0500

    Create Lisas-Workbook.cpp

---
+++ Implementation/Lisas-Workbook.cpp
@@ -0,0 +1,7 @@
+/*******************************************************
+ * Copyright (C) 2016 Haotian Wu
+ * 
+ * This file is NOT a solution.
+ *
+ * For the solution to "Lisa's Workbook", see "bear-and-workbook.cpp" in this folder.
+ *******************************************************/

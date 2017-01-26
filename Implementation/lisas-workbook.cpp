42640a0993f825e7fa6ca5b289d93620e5c691e2
Haotian Wu <buaawu@gmail.com>
2017-01-26 10:31:59 -0600

    Update lisas-workbook.cpp

--- Implementation/lisas-workbook.cpp
+++ Implementation/lisas-workbook.cpp
@@ -1,7 +1,43 @@
 /*******************************************************
  * Copyright (C) 2016 Haotian Wu
- * 
- * This file is NOT a solution.
  *
- * For the solution to "Lisa's Workbook", see "bear-and-workbook.cpp" in this folder.
+ * This file is the solution to the question:
+ * https://www.hackerrank.com/challenges/lisa-workbook
+ *
+ * Redistribution and use in source and binary forms are permitted.
  *******************************************************/
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <map>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// Just implement!
+
+int main() {
+    int n,k;
+    scanf("%d %d",&n,&k);
+    int arr[100];
+    for (int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    // lu[i] stores the lower bound and upper bound of question numbers on page i.
+    pair<int,int> lu[10001];
+    int curpage = 1;
+    for (int i=0;i<n;i++)
+    {
+        for (int j=0; j<(arr[i]+k-1)/k; j++)
+            lu[j+curpage] = make_pair(k*j+1, min(k*(j+1), arr[i]));
+        curpage += (arr[i]+k-1)/k;
+    }
+    int cnt = 0;
+    for (int i=1; i<curpage; i++)
+        if (lu[i].first <= i && i <= lu[i].second)
+            cnt++;
+    printf("%d\n",cnt);
+    return 0;
+}

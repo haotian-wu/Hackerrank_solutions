12f1640afac674c876421fc2c7bde09a7e05e6db
Haotian Wu <buaawu@gmail.com>
2017-02-04 00:38:26 -0600

    Create beautiful-triplets.cpp

---
+++ Implementation/beautiful-triplets.cpp
@@ -0,0 +1,8 @@
+/*******************************************************
+ * Copyright (C) 2017 Haotian Wu
+ * 
+ * This file is NOT a solution.
+ *
+ * For the solution to "Beautiful Triplets", see "beautiful-triplets-1.cpp", or 
+ * "beautiful-triplets-2.cpp", or "beautiful-triplets-3.cpp" in this folder, for three different methods.
+ *******************************************************/

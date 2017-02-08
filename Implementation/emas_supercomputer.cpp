523ce7e060f1092164a7ad895dfbd4fe6017f7b0
Haotian Wu <buaawu@gmail.com>
2017-02-08 15:01:05 -0600

    Create emas_supercomputer.cpp

---
+++ Implementation/emas_supercomputer.cpp
@@ -0,0 +1,7 @@
+/*******************************************************
+ * Copyright (C) 2017 Haotian Wu
+ * 
+ * This file is NOT a solution.
+ *
+ * For the solution to "Ema's Supercomputer", see "two-pluses.cpp" in this folder.
+ *******************************************************/

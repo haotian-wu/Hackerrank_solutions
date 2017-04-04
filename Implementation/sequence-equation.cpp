5714cabe2951284e9bc06808884b8fd828aa47f6
Haotian Wu <buaawu@gmail.com>
2017-04-04 16:35:44 -0500

    Create sequence-equation.cpp

---
+++ Implementation/sequence-equation.cpp
@@ -0,0 +1,7 @@
+/*******************************************************
+ * Copyright (C) 2017 Haotian Wu
+ * 
+ * This file is NOT a solution.
+ *
+ * For the solution to "Sequence Equation", see "permutation-equation.cpp" in this folder.
+ *******************************************************/
